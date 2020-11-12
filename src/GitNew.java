90b9ced6d1b912b9a27d39ce1dd06eebe09b9ff2
Abiel Menghistu <abielmenghistu@gmail.com>
2020-11-12 11:25:06 -0500

    This is check your head moment! The second commit in this proje4.

--- src/GitNew.java
+++ src/GitNew.java
@@ -1,5 +1,7 @@
 public class GitNew {
     public static void main(String[] args) {
         System.out.println("This is third day of Github trainning!");
+        // B21 has update the file B21project4.
+        // we are moving smooth.
     }
 }
